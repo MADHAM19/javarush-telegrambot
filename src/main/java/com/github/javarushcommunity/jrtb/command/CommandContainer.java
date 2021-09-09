67867e4876f7025ecd97c61d4fc2c0fded5bf30a
MADHAM19 <mad_cerber@mail.ru>
2021-09-09 12:55:12 +0600

    As a user, I want to add the group to the subscription (#14)
    * As a user, I want to add the group to the subscription
    
    * change url of postgres in properties

--- src/main/java/com/github/javarushcommunity/jrtb/command/CommandContainer.java
+++ src/main/java/com/github/javarushcommunity/jrtb/command/CommandContainer.java
@@ -1,10 +1,13 @@
 package com.github.javarushcommunity.jrtb.command;
 
+import com.github.javarushcommunity.jrtb.javarushclient.JavaRushGroupClient;
+import com.github.javarushcommunity.jrtb.service.GroupSubService;
 import com.github.javarushcommunity.jrtb.service.SendBotMessageService;
 import com.github.javarushcommunity.jrtb.service.TelegramUserService;
 import com.google.common.collect.ImmutableMap;
 
 import static com.github.javarushcommunity.jrtb.command.CommandName.*;
+
 
 /**
  * Container of the {@link Command}s, which are using for handling telegram commands.
@@ -14,7 +17,8 @@ public class CommandContainer {
     private final ImmutableMap<String, Command> commandMap;
     private final Command unknownCommand;
 
-    public CommandContainer(SendBotMessageService sendBotMessageService, TelegramUserService telegramUserService) {
+    public CommandContainer(SendBotMessageService sendBotMessageService, TelegramUserService telegramUserService,
+                            JavaRushGroupClient javaRushGroupClient, GroupSubService groupSubService) {
 
         commandMap = ImmutableMap.<String, Command>builder()
                 .put(START.getCommandName(), new StartCommand(sendBotMessageService, telegramUserService))
@@ -22,6 +26,7 @@ public class CommandContainer {
                 .put(HELP.getCommandName(), new HelpCommand(sendBotMessageService))
                 .put(NO.getCommandName(), new NoCommand(sendBotMessageService))
                 .put(STAT.getCommandName(), new StatCommand(sendBotMessageService, telegramUserService))
+                .put(ADD_GROUP_SUB.getCommandName(), new AddGroupSubCommand(sendBotMessageService, javaRushGroupClient, groupSubService))
                 .build();
 
         unknownCommand = new UnknownCommand(sendBotMessageService);
---
+++ src/main/resources/db/migration/V00002__created_groupsub_many_to_many.sql
@@ -0,0 +1,21 @@
+-- add PRIMARY KEY FOR users
+alter table users add PRIMARY KEY (chat_id);
+
+-- ensure that the tables with these names are removed before creating a new one.
+drop table IF EXISTS group_sub;
+drop table IF EXISTS group_x_user;
+
+create TABLE group_sub (
+   id INT,
+   title VARCHAR(100),
+   last_article_id INT,
+   PRIMARY KEY (id)
+);
+
+create TABLE group_x_user (
+   group_sub_id INT NOT NULL,
+   user_id VARCHAR(100) NOT NULL,
+   FOREIGN KEY (user_id) REFERENCES users(chat_id),
+   FOREIGN KEY (group_sub_id) REFERENCES group_sub(id),
+   CONSTRAINT group_x_user_user_id_group_sub_id_key UNIQUE (user_id, group_sub_id)
+);
